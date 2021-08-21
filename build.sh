bd78f1342ea29466aa79eb1d9bd457ea307828eb
rmacedo2021 <79383089+rmacedo2021@users.noreply.github.com>
2021-08-21 01:13:28 -0300

    teste

--- build.sh
+++ build.sh
@@ -1,69 +1,60 @@
 #!/usr/bin/env bash
 
-echo "##### ##### ##### VERIFICANDO PRE REQUISITOS ##### ##### #####"
+echo -e "##### ##### ##### VERIFICANDO PRE REQUISITOS ##### ##### #####\n\n"
 
-: '
-  Checa se a ferramenta make está disponível no sistema
-'
-if ! make -v &>/dev/null; then
-  echo "Comando make não está disponível."
-  echo "Este programa possui um job de compilação contido em um Makefile, porém, este depende do comando make para ser executado."
-  echo "Consulte o link para maiores detalhes https://www.gnu.org/software/make/"
-  exit 1
-fi
+#  Checa se a ferramenta make está disponível no sistema
 
-: '
-  Checa se o compilador da linguagem GoLang está instalado no sistema
-'
-if ! go version &>/dev/null; then
-  echo "Compilador GO não encontrado."
-  echo "O Makefile utiliza somente instruções de compilação válidas para a distribuição padrão do Go, portanto o GCC GO não é suportado."
-  echo "Consulte o link para baixar o compilador Go https://golang.org/dl/"
-  exit 1
-elif [ "$(go version | awk '{print $3}' | awk -F . '{print $2}')" -lt 16 ]; then
-  echo "A versão mínima requerida para compilar este programa é go1.16, por favor atualize seu ambiente de desenvolvimento."
-  echo "Consulte o link para baixar uma versão atualizada: https://golang.org/dl/"
-  echo "Versão do compilador Go encontrada: $(go version)"
-  exit 1
-fi
-
-: '
-  Checa se o sistema possui o Google Chrome instalado.
-   É necessaŕio pois o chromedp utiliza-o no modo headless para efetuar o scrapping da página de administração do modem.
-'
-if ! google-chrome --version &>/dev/null; then
-  echo "Navegador Google Chrome não encontrado."
-  echo "A ferramenta de scrapping depende de uma instalação desse navegador e sua ausência tornam inúteis os binários compilados"
-  echo "Consulte o link para baixá-lo https://www.google.com/intl/pt-BR/chrome/"
-  exit 1
-fi
-
-echo "##### ##### ##### PRE REQUISITOS SATISFEITOS ##### ##### #####"
-
-echo
-
-echo "##### ##### ##### COMPILANDO OS BINÁRIOS PARA WINDOWS, lINUX E MACOS ##### ##### #####"
-
-: '
-  Exefuta os jobs de compilação presentes no arquivo Makefile.
-'
-make "-j$(cat /proc/cpuinfo | grep siblings | awk 'NR==1 { print $NF }')" || {
-  echo 'FALHA NA COMPILAÇÃO DO PROGRAMA. REVISE AS MENSAGENS DE ERRO APRESENTADAS NO TERMINAL E TENTE NOVAMENTE.'
+command -v make 1>- || {
+  printf "Comando make não está disponível.
+  Este programa possui um job de compilação contido em um Makefile, porém, este depende do comando make para ser executado.
+  Consulte o link para maiores detalhes https://www.gnu.org/software/make/\n"
   exit 1
 }
 
-echo "##### ##### ##### BINÁRIOS COMPILADOS ##### ##### #####"
+#  Checa se o compilador da linguagem GoLang está instalado no sistema
 
-echo
+command -v go 1>- && {
+  [[ $(go version) =~ ([0-9]\.?){3} ]] && {
+  ((${BASH_REMATCH[2]} < 16)) && {
+    echo -e "A versão mínima requerida para compilar este programa é go1.16, por favor atualize seu ambiente de desenvolvimento.
+    Consulte o link para baixar uma versão atualizada: https://golang.org/dl/
+    Versão do compilador Go encontrada: ${BASH_REMATCH[0]}\n"
+ }
+}
+} || {
+  echo -e "Compilador GO não encontrado.
+  O Makefile utiliza somente instruções de compilação válidas para a distribuição padrão do Go, portanto o GCC GO não é suportado.
+  Consulte o link para baixar o compilador Go https://golang.org/dl/\n"
+  exit 1
+}
 
-echo "##### ##### ##### LISTANDO OS ARQUIVOS COMPILADOS ##### ##### #####"
+#  Checa se o sistema possui o Google Chrome instalado.
+#  É necessaŕio pois o chromedp utiliza-o no modo headless para efetuar o scrapping da página de administração do modem.
 
-cd bin/ || return
-echo "Arquivos compilados no diretório: $(pwd)"
-ls -lh
+command -v google-chrome 1>- || {
+  echo -e "Navegador Google Chrome não encontrado.
+  A ferramenta de scrapping depende de uma instalação desse navegador e sua ausência tornam inúteis os binários compilados
+  Consulte o link para baixá-lo https://www.google.com/intl/pt-BR/chrome/\n"
+  exit 1
+}
 
-echo
+echo -e "\n##### ##### ##### PRE REQUISITOS SATISFEITOS ##### ##### #####
 
-echo "##### ##### ##### TRABALHO FINALIZADO ##### ##### #####"
+##### ##### ##### COMPILANDO OS BINÁRIOS PARA WINDOWS, lINUX E MACOS ##### ##### #####\n\n"
+
+#  Exefuta os jobs de compilação presentes no arquivo Makefile.
+
+make "-j$(grep siblings /proc/cpuinfo | awk 'NR==1 { print $NF }')" || {
+  echo -e 'FALHA NA COMPILAÇÃO DO PROGRAMA. REVISE AS MENSAGENS DE ERRO APRESENTADAS NO TERMINAL E TENTE NOVAMENTE.\n' 
+  exit 1
+}
+
+echo -e "##### ##### ##### BINÁRIOS COMPILADOS ##### ##### #####
+
+##### ##### ##### LISTANDO OS ARQUIVOS COMPILADOS ##### ##### #####\n"
+
+[[ -a /bin ]] && cd /bin
+echo -e "Arquivos compilados no diretório: ${PWD}\n\n
+##### ##### ##### TRABALHO FINALIZADO ##### ##### #####\n"
 
 exit 0
